1e7e32dbdae17b73212af60f84d6c9e5e4d5515b
MariaFjodorowa <laks.salmon@gmail.com>
2025-03-14 22:04:47 +0100

    link to models, batch prediction

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 [![arXiv](https://img.shields.io/badge/arXiv-2502.06692-b31b1b.svg)](http://arxiv.org/abs/2502.06692)
 
-Data and code (still being added) for the paper Multi-label Scandinavian Language Identification (SLIDE) (accepted to RESOURCEFUL-2025).
+Data and code (still being added) for the paper Multi-label Scandinavian Language Identification (SLIDE) (presented at [RESOURCEFUL-2025](https://resourceful-workshop.github.io/resourceful-2025/papers.html)).
 
+##### [Models on HuggingFace](https://huggingface.co/collections/ltg/slide-67d4538eac9736a6f068bf2a) and [how to use them](src/usage_example.py)
 
---
+++ src/usage_example.py
@@ -0,0 +1,55 @@
+import os
+
+import torch
+from torch.cuda import is_available
+from transformers import AutoModelForSequenceClassification
+
+from custom_tokenizer import CustomTokenizer
+
+SIGMOID_THRESHOLD = 0.5
+
+if __name__ == '__main__':
+    model_name = 'ltg/SLIDE-base'
+    device = torch.device('cuda') if is_available() else torch.device('cpu')
+    model = AutoModelForSequenceClassification.from_pretrained(
+        model_name,
+        trust_remote_code=True,
+    ).to(device)
+    model.eval()
+    tokenizer = CustomTokenizer(
+        model_name,
+        cache_dir=os.path.expanduser('~/.cache/huggingface/transformers/hub/')
+    )
+
+    texts = [
+        'En dag i livet',
+        'Jag vill ha deg',
+        'Jeg er hvalrossen',
+        'Denne fuglen har flydd',
+        'not a Scandinavian text at all',
+        'i sit berømte værk die normen und ihre bertretung i',
+    ]
+
+    with torch.no_grad():
+        batch = tokenizer(texts, padding=True)
+        batch['input_ids'] = torch.Tensor(batch['input_ids']).long().to(
+            device
+        )
+        batch['attention_mask'] = torch.Tensor(batch['attention_mask']).long(
+        ).to(device)
+        batch['token_type_ids'] = torch.Tensor(batch['token_type_ids']).long(
+        ).to(device)
+        out = torch.sigmoid(model(**batch).logits)
+        greater = out > SIGMOID_THRESHOLD
+        indices = greater.nonzero()
+        text_indices = indices[:, 0].unique()
+        n_samples = torch.arange(end=out.shape[0]).to(device)
+
+        for n_sample in n_samples:
+            print(texts[n_sample])
+            if n_sample in text_indices:
+                this_sample_indices = indices[torch.where(indices[:, 0]==n_sample)][:, 1]
+                print([model.config.id2label[idx.item()] for idx in this_sample_indices])
+            else:
+                print(['other'])
+            print('---------')
